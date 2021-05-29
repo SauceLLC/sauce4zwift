d05b29313e255d8eaec0e4bcd3e129f25415051f
Justin Mayfield <tooker@gmail.com>
2021-05-29 12:20:34 -0600

    Checkpoint on chat widget

--- index.js
+++ index.js
@@ -356,12 +356,12 @@ function makeFloatingWindow(page, options={}) {
     const win = new BrowserWindow({
         width: 400,
         height: 300,
-        transparent: true,
+        //transparent: true,
         //frame: false,
-        titleBarStyle: 'customButtonsOnHover',  // best so far  dragging is possible but difficult with top bar and wrong cursor
+        //titleBarStyle: 'customButtonsOnHover',  // best so far  dragging is possible but difficult with top bar and wrong cursor
         x: windowOfft,
         y: windowOfft,
-        alwaysOnTop: true,
+        //alwaysOnTop: true,
         resizable: true,
         webPreferences: {
             nodeIntegration: false,
@@ -376,9 +376,8 @@ function makeFloatingWindow(page, options={}) {
 
 function createWindow(monitor) {
     const watchingWin = makeFloatingWindow('watching.html', {width: 250, height: 238, x: 14, y: 60});
-    //const nearbyWin = makeFloatingWindow('nearby.html', {width: 240, height: 600, x: 980, y: 318});
-    const nearbyWin = makeFloatingWindow('nearby.html', {width: 500, height: 400, x: 780, y: 418});
-    const groupsWin = makeFloatingWindow('groups.html', {width: 500, height: 400, x: 270, y: 418});
+    //const nearbyWin = makeFloatingWindow('nearby.html', {width: 500, height: 400, x: 780, y: 418});
+    //const groupsWin = makeFloatingWindow('groups.html', {width: 500, height: 400, x: 270, y: 418});
     const chatWin = makeFloatingWindow('chat.html', {width: 300, height: 400, x: 780, y: 100});
 
     //app.dock.hide();
