dc2c32863af139b496f94719b2847b1b1ce0b3f5
Justin Mayfield <tooker@gmail.com>
2021-12-24 04:56:36 -0700

    rotated power fields

--- pages/watching.js
+++ pages/watching.js
@@ -9,10 +9,10 @@ function humanNumber(num, fallback='-') {
 }
 
 
-function rotateField(label, fields, cur) {
+function rotateField(label, fields, cur, defIndex) {
     let i;
     if (!cur) {
-        i = localStorage.getItem(label);
+        i = localStorage.getItem(label) || defIndex;
     } else {
         i = fields.indexOf(cur) + 1;
         localStorage.setItem(label, i);
@@ -35,71 +35,102 @@ function makePeakField(period) {
             }
             const ago = (Date.now() - o.ts) / 1000;
             return `${label}<br/><small>${sauce.locale.humanDuration(ago)} ago</small>`;
-        }
+        },
+        key: () => `Peak ${sauce.locale.humanDuration(period, {short: true})}`
     };
 }
 
 
 async function main() {
     const content = document.querySelector('#content');
-    const pwrCurValueEl = content.querySelector('.power .current .value');
-    const pwrCurLabelEl = content.querySelector('.power .current .label');
+    const pwrMainValueEl = content.querySelector('.power .main .value');
+    const pwrMainLabelEl = content.querySelector('.power .main .label');
+    const pwrUpperValueEl = content.querySelector('.power .upper .value');
+    const pwrUpperKeylEl = content.querySelector('.power .upper .key');
+    const pwrLowerValueEl = content.querySelector('.power .lower .value');
+    const pwrLowerKeyEl = content.querySelector('.power .lower .key');
     const hrCurEl = content.querySelector('.hr .current .value');
     const cadCurEl = content.querySelector('.cadence .current .value');
     const draftCurEl = content.querySelector('.draft .current .value');
-    const pwrAvgEl = content.querySelector('.power .avg .value');
     const hrAvgEl = content.querySelector('.hr .avg .value');
     const cadAvgEl = content.querySelector('.cadence .avg .value');
     const draftAvgEl = content.querySelector('.draft .avg .value');
-    const pwrMaxEl = content.querySelector('.power .max .value');
     const hrMaxEl = content.querySelector('.hr .max .value');
     const powerFields = [{
         value: x => x.power,
         label: () => 'watts',
+        key: () => 'Watts',
+    }, {
+        value: x => x.stats.powerMax,
+        label: () => 'max',
+        key: () => 'Max',
     }, {
         value: x => x.stats.powerAvg,
-        label: () => 'Roll eAVG',
+        label: () => 'avg (elapsed)',
+        key: () => 'eAvg',
     }, {
         value: x => x.stats.powerAvgActive,
-        label: () => 'Roll aAVG',
+        label: () => 'avg (active)',
+        key: () => 'aAvg',
     }, {
         value: x => x.stats.powerNP,
-        label: () => 'NP',
+        label: () => 'np',
+        key: () => 'NP',
     }, {
         value: x => x.stats.power5s,
         label: () => '5s watts',
+        key: () => '5s',
     }, {
         value: x => x.stats.power30s,
         label: () => '30s watts',
+        key: () => '30s',
     },
         makePeakField(5),
-        makePeakField(30),
         makePeakField(60),
-        makePeakField(120),
         makePeakField(300),
-        makePeakField(1200),
-        makePeakField(3600),
     ];
-    let powerField = rotateField('power', powerFields);
-    content.querySelector('.power .current').addEventListener('click', ev => {
-        powerField = rotateField('power', powerFields, powerField);
+    let lastDraw = 0;
+    let powerFieldMain = rotateField('power-main', powerFields, null, 0);
+    content.querySelector('.power .main').addEventListener('click', ev => {
+        powerFieldMain = rotateField('power-main', powerFields, powerFieldMain);
+        lastDraw = 0;
     });
+    let powerFieldLower = rotateField('power-lower', powerFields, null, 1);
+    content.querySelector('.power .lower').addEventListener('click', ev => {
+        powerFieldLower = rotateField('power-lower', powerFields, powerFieldLower);
+        lastDraw = 0;
+    });
+    let powerFieldUpper = rotateField('power-upper', powerFields, null, 2);
+    content.querySelector('.power .upper').addEventListener('click', ev => {
+        powerFieldUpper = rotateField('power-upper', powerFields, powerFieldUpper);
+        lastDraw = 0;
+    });
+
+
     sauce.subscribe('watching', watching => {
+        const ts = Date.now();
+        const sinceLast = ts - lastDraw;
+        if (sinceLast < 700) {
+            return;
+        }
+        lastDraw = ts;
         const stats = watching.stats;
 
-        console.log('adsf', watching.power);
-        pwrCurValueEl.textContent = humanNumber(powerField.value(watching));
-        pwrCurLabelEl.innerHTML = powerField.label(watching);
+        pwrMainValueEl.textContent = humanNumber(powerFieldMain.value(watching));
+        pwrMainLabelEl.innerHTML = powerFieldMain.label(watching);
+        pwrUpperValueEl.textContent = humanNumber(powerFieldUpper.value(watching));
+        pwrUpperKeylEl.innerHTML = powerFieldUpper.key(watching);
+        pwrLowerValueEl.textContent = humanNumber(powerFieldLower.value(watching));
+        pwrLowerKeyEl.innerHTML = powerFieldLower.key(watching);
+
         hrCurEl.textContent = humanNumber(watching.heartrate || null);
         cadCurEl.textContent = humanNumber(watching.cadence);
         draftCurEl.textContent = humanNumber(watching.draft);
 
-        pwrAvgEl.textContent = humanNumber(stats.powerSum / stats.powerDur);
         hrAvgEl.textContent = humanNumber(stats.hrSum / stats.hrDur);
         cadAvgEl.textContent = humanNumber(stats.cadenceSum / stats.cadenceDur);
         draftAvgEl.textContent = humanNumber(stats.draftSum / stats.draftDur);
 
-        pwrMaxEl.textContent = humanNumber(stats.powerMax || null);
         hrMaxEl.textContent = humanNumber(stats.hrMax || null);
     });
 }
