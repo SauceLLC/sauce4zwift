f160f7a93029ad665203b107539e85c970451fb1
Justin Mayfield <tooker@gmail.com>
2021-12-28 15:28:16 -0700

    Modulize overview window

--- pages/overview.mjs
+++ pages/overview.mjs
@@ -1,15 +1,21 @@
 /* global sauce */
 
+import sauce from '../shared/sauce/index.mjs';
+import common from './common.mjs';
+
 const L = sauce.locale;
+const H = L.human;
 
 
 function shortDuration(x) {
-    return L.humanDuration(x, {short: true});
+    return H.duration(x, {short: true});
 }
 
+
 async function main() {
+    common.initInteractionListeners();
     const content = document.querySelector('#content');
-    const renderer = new sauce.Renderer(content, {fps: 1});
+    const renderer = new common.Renderer(content, {fps: 1});
     renderer.addRotatingFields({
         mapping: [{
             id: 'social',
@@ -22,52 +28,52 @@ async function main() {
             default: 2
         }],
         fields: [{
-            value: x => L.humanNumber(x.rideons),
+            value: x => H.number(x.rideons),
             key: () => 'Ride Ons',
         }, {
-            value: x => L.humanNumber(x.kj),
+            value: x => H.number(x.kj),
             key: () => 'Energy',
             unit: () => 'kJ',
         }, {
-            value: x => L.humanNumber(x.speed), // XXX need backend support
+            value: x => H.number(x.speed), // XXX need backend support
             key: () => 'Speed <small>(avg)</small>',
             unit: () => 'kph',
         }, {
-            value: x => L.humanNumber(x.stats.powerMax),
+            value: x => H.number(x.stats.powerMax),
             key: () => 'Power <small>(max)</small>',
             unit: () => 'w',
         }, {
-            value: x => L.humanNumber(x.stats.powerAvg),
+            value: x => H.number(x.stats.powerAvg),
             key: () => 'Power <small>(avg)</small>',
             unit: () => 'w',
         }, {
-            value: x => L.humanNumber(x.stats.powerNP),
+            value: x => H.number(x.stats.powerNP),
             key: () => 'NP',
         }, {
-            value: x => L.humanNumber(x.stats.power5s),
+            value: x => H.number(x.stats.power5s),
             key: () => `Power <small>(${shortDuration(5)})</small>`,
             unit: () => 'w',
         }, {
-            value: x => L.humanNumber(x.stats.power30s),
+            value: x => H.number(x.stats.power30s),
             key: () => `Power <small>(${shortDuration(30)})</small>`,
             unit: () => 'w',
         }],
     });
 
     content.querySelector('.button.show').addEventListener('click', () => {
-        sauce.electronTrigger('showAllWindows');
+        common.electronTrigger('showAllWindows');
         document.documentElement.classList.toggle('hidden');
     });
     content.querySelector('.button.hide').addEventListener('click', () => {
-        sauce.electronTrigger('hideAllWindows');
+        common.electronTrigger('hideAllWindows');
         document.documentElement.classList.toggle('hidden');
     });
     content.querySelector('.button.quit').addEventListener('click', () => {
-        sauce.electronTrigger('quit');
+        common.electronTrigger('quit');
     });
  
     let lastUpdate = 0;
-    sauce.subscribe('watching', watching => {
+    common.subscribe('watching', watching => {
         renderer.setData(watching);
         const ts = Date.now();
         if (ts - lastUpdate > 500) {
