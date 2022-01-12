4e3f4ee5ba7fead6aaa87c70d9411e74d54f56bd
Justin Mayfield <tooker@gmail.com>
2022-01-11 22:41:26 -0700

    Sortable fields for nearby

--- pages/nearby.mjs
+++ pages/nearby.mjs
@@ -10,11 +10,11 @@ function spd(v) {
 }
 
 function pwr(v) {
-    return v ? `${num(v)}<small>w</small>`: v == null ? '-' : v;v
+    return v ? `${num(v)}<small>w</small>`: v == null ? '-' : v;
 }
 
 function hr(v) {
-    return v ? `${num(v)}<small>bpm</small>` : '-';;
+    return v ? `${num(v)}<small>bpm</small>` : '-';
 }
 
 function clearSelection() {
@@ -25,38 +25,43 @@ async function main() {
     common.initInteractionListeners();
 
     const fields = {
-        'ID': x => x.athleteId,
+        'ID': {get: x => x.athleteId, fmt: x => x},
 
-        'Gap': x => `${num(x.gap)}s`,
+        'Gap': {get: x => x.gap, fmt: x => `${num(x)}s`},
 
-        'Pwr': x => pwr(x.power),
-        '5s Pwr': x => pwr(x.stats.power.smooth['5']),
-        '1m Pwr': x => pwr(x.stats.power.smooth['60']),
-        '5m Pwr': x => pwr(x.stats.power.smooth['300']),
-        'Avg Pwr': x => pwr(x.stats.power.avg),
-        'NP Pwr': x => pwr(x.stats.power.np),
-        'Max Pwr': x => pwr(x.stats.power.max),
-        '5s Peak Pwr': x => pwr(x.stats.power.peaks[5].avg),
-        '1m Peak Pwr': x => pwr(x.stats.power.peaks[60].avg),
-        '5m Peak Pwr': x => pwr(x.stats.power.peaks[300].avg),
+        'Pwr': {get: x => x.power, fmt: pwr},
+        '5s Pwr': {get: x => x.stats.power.smooth['5'], fmt: pwr},
+        '1m Pwr': {get: x => x.stats.power.smooth['60'], fmt: pwr},
+        '5m Pwr': {get: x => x.stats.power.smooth['300'], fmt: pwr},
+        'Avg Pwr': {get: x => x.stats.power.avg, fmt: pwr},
+        'NP Pwr': {get: x => x.stats.power.np, fmt: pwr},
+        'Max Pwr': {get: x => x.stats.power.max, fmt: pwr},
+        '5s Peak Pwr': {get: x => x.stats.power.peaks[5].avg, fmt: pwr},
+        '1m Peak Pwr': {get: x => x.stats.power.peaks[60].avg, fmt: pwr},
+        '5m Peak Pwr': {get: x => x.stats.power.peaks[300].avg, fmt: pwr},
 
-        'Spd': x => spd(x.speed),
-        '1m Spd': x => spd(x.stats.speed.smooth[60]),
-        'Avg Spd': x => spd(x.stats.speed.avg),
-        '1m Peak Spd': x => spd(x.stats.speed.peaks[60].avg),
+        'Spd': {get: x => x.speed, fmt: spd},
+        '1m Spd': {get: x => x.stats.speed.smooth[60], fmt: spd},
+        'Avg Spd': {get: x => x.stats.speed.avg, fmt: spd},
+        '1m Peak Spd': {get: x => x.stats.speed.peaks[60].avg, fmt: spd},
 
-        'HR': x => hr(x.heartrate),
-        '1m HR': x => hr(x.stats.hr.smooth[60]),
-        'Avg HR': x => hr(x.stats.hr.avg),
-        '1m Peak HR': x => hr(x.stats.hr.peaks[60].avg),
+        'HR': {get: x => x.heartrate, fmt: hr},
+        '1m HR': {get: x => x.stats.hr.smooth[60], fmt: hr},
+        'Avg HR': {get: x => x.stats.hr.avg, fmt: hr},
+        '1m Peak HR': {get: x => x.stats.hr.peaks[60].avg, fmt: hr},
     };
+
+    let sortByCol = 1;
+    let sortByDir = 1;
+    let hiCol = sortByCol;
+    let hiRow;
 
     const table = document.querySelector('#content table');
     const tbody = table.querySelector('tbody');
     const theadRow = table.querySelector('thead tr');
-    theadRow.innerHTML = Object.keys(fields).map((x, i) => `<td data-id="${i}">${x}</td>`).join('');
+    theadRow.innerHTML = Object.keys(fields).map((x, i) =>
+        `<td data-id="${i}" class="${hiCol === i ? 'hi' : ''}">${x}</td>`).join('');
  
-    let hiRow;
     tbody.addEventListener('dblclick', ev => {
         const row = ev.target.closest('tr');
         if (row) {
@@ -71,30 +76,36 @@ async function main() {
         }
     });
 
-    let hiCol;
-    theadRow.addEventListener('dblclick', ev => {
+    theadRow.addEventListener('click', ev => {
         const col = ev.target.closest('td');
         if (col) {
-            clearSelection();
-            hiCol = Number(col.dataset.id);
+            const id = Number(col.dataset.id);
+            if (id === sortByCol) {
+                sortByDir = -sortByDir;
+            }
+            hiCol = sortByCol = id;
             for (const td of table.querySelectorAll('td.hi')) {
                 td.classList.remove('hi');
             }
             for (const td of table.querySelectorAll(`td[data-id="${hiCol}"]`)) {
                 td.classList.add('hi');
             }
+            render();
         }
     });
+
     let pause;
     document.addEventListener('selectionchange', ev => pause = !!getSelection().toString());
 
     let nextAnimFrame;
     let frames = 0;
-    common.subscribe('nearby', async nearby => {
+    let nearby;
+    function render() {
         if (!nearby.length || pause || document.hidden) {
             return;
         }
-        nearby.sort((a, b) => a.gap - b.gap);
+        const sortFn = Object.values(fields)[sortByCol].get;
+        nearby.sort((a, b) => (sortFn(a) - sortFn(b)) * sortByDir);
         const html = nearby.map(x => {
             const classes = [];
             if (x.watching) {
@@ -105,8 +116,8 @@ async function main() {
             }
             return `
                 <tr data-id="${x.athleteId}" class="${classes.join(' ')}">
-                    ${Object.values(fields).map((fmt, i) =>
-                        `<td data-id="${i}" class="${hiCol === i ? 'hi' : ''}">${fmt(x)}</td>`
+                    ${Object.values(fields).map(({get, fmt}, i) =>
+                        `<td data-id="${i}" class="${hiCol === i ? 'hi' : ''}">${fmt(get(x))}</td>`
                     ).join('')}
                 </tr>
             `;
@@ -127,6 +138,10 @@ async function main() {
                 });
             }
         });
+    }
+    common.subscribe('nearby', _nearby => {
+        nearby = _nearby;
+        render();
     });
 }
 
