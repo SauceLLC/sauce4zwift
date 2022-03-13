767935643b0e110ddd45abd632ba9fd7360a3c2c
Justin Mayfield <tooker@gmail.com>
2022-03-13 01:12:40 -0700

    Fix notarize

--- build/scripts/notarize.js
+++ build/scripts/notarize.js
@@ -2,20 +2,21 @@ require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
-  const appName = context.packager.appInfo.productFilename;
-  const appleId = process.env.APPLE_ID;
-  const appleIdPassword = process.env.APPLE_ID_PASSWORD
-  if (!appleId || !appleIdPassword) {
-    throw new Error("APPLE_ID env not set");
-  }
-  return await notarize({
-    appBundleId: 'io.saucellc.sauce4zwift',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId,
-    appleIdPassword,
-  });
+    const {electronPlatformName, appOutDir} = context;
+    const skip = process.env.SKIP_NOTARIZE;
+    if (electronPlatformName !== 'darwin' || skip) {
+        return;
+    }
+    const appName = context.packager.appInfo.productFilename;
+    const appleId = process.env.APPLE_ID;
+    const appleIdPassword = process.env.APPLE_ID_PASSWORD
+    if (!appleId || !appleIdPassword) {
+        throw new Error("APPLE_ID env not set");
+    }
+    return await notarize({
+        appBundleId: 'io.saucellc.sauce4zwift',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId,
+        appleIdPassword,
+    });
 };
