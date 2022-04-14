08c2dc4a5c2ae0505e93ba1978856cfe6a61bc60
Justin Mayfield <tooker@gmail.com>
2022-04-14 17:00:50 -0600

    Use dumbed down windows safe file save

--- src/storage.mjs
+++ src/storage.mjs
@@ -1,9 +1,11 @@
 import path from 'node:path';
+import process from 'node:process';
 import fs from 'node:fs/promises';
 import {createRequire} from 'node:module';
 const require = createRequire(import.meta.url);
 const {app} = require('electron');
 
+const brokenRename = process.platform === 'win32';
 
 function getFilePath(id) {
     return path.join(app.getPath('userData'), `storage-${id}.json`);
@@ -28,17 +30,37 @@ async function load(id, defaultFile) {
 }
 
 
-async function save(id, data) {
-    const file = getFilePath(id);
-    const tmpFile = file + `.tmp.${Date.now()}.${Math.round(Math.random() * 10000000)}`;
-    const serialized = JSON.stringify(data);
-    const f = await fs.open(tmpFile, 'w');
-    try {
-        await f.writeFile(serialized);
-    } finally {
-        await f.close();
-    }
-    await fs.rename(tmpFile, file);
+let save;
+if (brokenRename) {
+    const pendingSaves = new Map();
+    save = async function(id, data) {
+        const file = getFilePath(id);
+        const serialized = JSON.stringify(data);
+        const pending = pendingSaves.get(id) || Promise.resolve();
+        const saving = pending.finally(async () => {
+            const f = await fs.open(file, 'w');
+            try {
+                await f.writeFile(serialized);
+            } finally {
+                await f.close();
+            }
+        });
+        pendingSaves.set(id, saving);
+        await saving;
+    };
+} else {
+    save = async function(id, data) {
+        const file = getFilePath(id);
+        const serialized = JSON.stringify(data);
+        const tmpFile = file + `.tmp.${Date.now()}.${Math.round(Math.random() * 100000)}`;
+        const f = await fs.open(tmpFile, 'w');
+        try {
+            await f.writeFile(serialized);
+        } finally {
+            await f.close();
+        }
+        await fs.rename(tmpFile, file);
+    };
 }
 
 export default {load, save};
