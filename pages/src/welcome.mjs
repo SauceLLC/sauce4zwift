0e28497f286c1c304dd017aec227b0d0ddbf06f3
Justin Mayfield <tooker@gmail.com>
2022-05-31 01:02:09 -0600

    Real version in splash

--- pages/src/welcome.mjs
+++ pages/src/welcome.mjs
@@ -5,4 +5,6 @@ export async function main() {
     common.initInteractionListeners();
     document.documentElement.classList.add('animate');
     setTimeout(() => document.documentElement.classList.add('fadeout'), 8000);
+    const version = await common.rpc.getVersion();
+    document.querySelector('footer').textContent = `v${version}`;
 }
