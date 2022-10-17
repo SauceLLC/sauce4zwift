14dcf0eaee5af48cd7cb8d9d7ba86a42d65f87fe
Justin Mayfield <tooker@gmail.com>
2022-10-16 19:06:07 -0600

    Frank 3.0 final

--- pages/src/overview.mjs
+++ pages/src/overview.mjs
@@ -422,7 +422,6 @@ async function frank() {
     bubble.classList.add('great-and-powerful-bubble');
     const words = document.createElement('div');
     words.classList.add('words');
-    bubble.innerHTML = '<span class="rightmask"></span>';
     bubble.append(words)
     words.textContent = 'The great and powerful Frank approves of your life choices!!!';
     const aud = document.createElement('audio');
