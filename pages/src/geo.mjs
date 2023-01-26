69d97bf152b55eafb83778e0413db50aad259a0a
Justin Mayfield <tooker@gmail.com>
2023-01-25 18:00:03 -0700

    Checkpoint geo
    This is in total disarray but I need to commit more

--- pages/src/geo.mjs
+++ pages/src/geo.mjs
@@ -1,7 +1,7 @@
 import * as sauce from '../../shared/sauce/index.mjs';
 import * as common from './common.mjs';
 import * as ec from '../deps/src/echarts.mjs';
-window.echarts = Object.fromEntries(Object.entries(ec));
+//window.echarts = Object.fromEntries(Object.entries(ec));
 import * as theme from './echarts-sauce-theme.mjs';
 
 ec.registerTheme('sauce', theme.getTheme('dynamic'));
@@ -12,7 +12,15 @@ let imperial = !!common.storage.get('/imperialUnits');
 L.setImperial(imperial);
 
 
-async function createElevationProfile() {
+function vectorDistance(a, b) {
+    const xd = b[0] - a[0];
+    const yd = b[1] - a[1];
+    const zd = b[2] - a[2];
+    return Math.sqrt(xd * xd + yd * yd + zd * zd);
+}
+
+
+async function createElevationProfile(renderer) {
     const chart = ec.init(document.querySelector('#content .elevation-profile'), 'sauce',
         {renderer: 'svg'});
     chart.setOption({
@@ -32,16 +40,13 @@ async function createElevationProfile() {
             type: 'inside',
         }],
         yAxis: {
+            show: false,
             type: 'value',
             min: x => Math.max(0, x.min - 20),
             max: x => Math.max(x.max, x.min + 200),
-            axisLabel: {
-                formatter: x => H.elevation(x, {suffix: true})
-            },
         },
         series: [{
             name: 'Elevation',
-            smooth: 0.5,
             type: 'line',
             symbol: 'none',
             areaStyle: {},
@@ -49,125 +54,36 @@ async function createElevationProfile() {
                 x: 0,
                 y: 1,
                 tooltip: [0, 1, 2]
+            },
+            markLine: {
+                symbol: 'none',
+                label: {
+                    position: 'start',
+                    distance: 10,
+                    formatter: x => H.elevation(x.value, {suffix: true}),
+                },
+                data: [{
+                    type: 'min',
+                }, {
+                    type: 'max',
+                }]
             }
         }]
     });
-    return chart;
-}
-
-
-async function createMapCanvas() {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    const img = document.createElement('img');
-    const world = (await (await fetch('/shared/deps/data/worldlist.json')).json()).find(x => x.courseId === 9);
-    const imgLoad = new Promise(resolve => img.addEventListener('load', resolve));
-    img.src = '/pages/images/maps/world-5.png';
-    await imgLoad;
-    const width = canvas.width = img.width;
-    const height = canvas.height = img.height;
-    ctx.drawImage(img, 0, 0, width, height);
-    document.querySelector('#content .map-canvas').append(canvas);
-    return {
-        doSomething: () => null,
-    };
-}
-
-
-async function createMapChart() {
-    const chart = ec.init(document.querySelector('#content .map-chart'));
-    chart.setOption({
-        //grid: {top: 0, left: 0, right: 0, bottom: 0},
-        legend: {show: true},
-        animation: false,
-        tooltip: {},
-        graphic: [{
-            type: 'image',
-            id: 'background',
-            left: chart.getWidth() * 0.115,
-            top: chart.getWidth() * 0.1,
-            z: -10,
-            bounding: 'raw',
-            style: {
-                image: '/pages/images/maps/world-5.png',
-                width: chart.getWidth() * 0.8,
-                height: chart.getHeight() * 0.8,
-            }
-        }],
-        xAxis: {
-            //show: false,
-            min: -450000,
-            max: 750000,
-        },
-        yAxis: {
-            //show: false,
-            min: -450000,
-            max: 750000
-        },
-        series: [{
-            type: 'scatter',
-            coordinateSystem: 'cartesian2d',
-            symbolSize: 6,
-            nolabel: {
-                position: 'right',
-                fontSize: 16,
-                show: true,
-                color: '#fff',
-                formatter: ({data}) => {
-                    const x = data.obj;
-                    const a = x.athlete;
-                    if (x.watching) {
-                        return `${a && a.initials}: ${Math.round(x.state.roadCompletion / 1000000 * 100)}%`;
-                    } else {
-                        return `${a && a.initials}: ${H.timer(Math.abs(x.gap))} ${Math.round(x.state.roadCompletion / 1000000 * 100)}%`;
-                    }
-                }
-            },
-            itemStyle: {
-                color: ({data}) => {
-                    const x = data.obj;
-                    const a = x.athlete;
-                    if (x.watching) {
-                        return '#33c';
-                    } else if (a && a.type === 'PACER_BOT') {
-                        return '#3c3';
-                    }
-                    return '#fffc';
-                }
-            },
-        }],
-    });
-    return chart;
-}
-
-
-export async function main() {
-    common.initInteractionListeners();
-    const worldList = await (await fetch('/shared/deps/data/worldlist.json')).json();
-    const elevationProfile = await createElevationProfile();
-    //const mapChart = await createMapChart();
-    //const mapCanvas = await createMapCanvas();
-    const content = document.querySelector('#content');
-    const renderer = new common.Renderer(content);
     let courseId;
     let roads;
     let road;
     let reverse;
-    let distances;
-    let gaps;
-    let grades;
     let worldMeta;
-    let nodes;
     let markAnimationDuration;
-    renderer.addCallback(async nearby => {
-        if (!nearby) {
+    const worldList = await (await fetch('/shared/deps/data/worldlist.json')).json();
+    renderer.addCallback(async _nearby => {
+        if (!_nearby || !_nearby.length) {
             return;
         }
-        /*mapChart.setOption({series: [{
-            data: nearby.map(x => ({value: [x.state.x, x.state.y], obj: x}))
-        }]});*/
-        nearby.sort((a, b) => a.athleted - b.athleteId);
-        nearby.sort((a, b) => a.watching ? 1 : b.watching ? -1 : 0);
+        const nearby = Array.from(_nearby);
+        nearby.sort((a, b) => a.athleteId - b.athleteId);  // stablize by athlete not gap.
+        nearby.sort((a, b) => a.watching ? 1 : b.watching ? -1 : 0); // put Watching mark on top
         const watching = nearby.find(x => x.watching);
         if (watching.state.courseId !== courseId) {
             courseId = watching.state.courseId;
@@ -179,42 +95,28 @@ export async function main() {
         if (!road || watching.state.roadId !== road.id || reverse !== watching.state.reverse) {
             road = roads[watching.state.roadId];
             reverse = watching.state.reverse;
-            nodes = Array.from(road.nodes).map(({pos}) => [
-                pos[0] / 100, // meters
-                pos[1] / 100, // meters
-                (pos[2] + worldMeta.waterPlaneLevel) / 100 * worldMeta.physicsSlopeScale + 
-                    worldMeta.altitudeHackOffset
-            ]);
-            if (reverse) {
-                nodes.reverse();
-            }
-            gaps = nodes.map((x, i) => i ? common.coordDistance(x, nodes[i - 1]) : 0);
-            distances = [];
-            gaps.forEach((x, i) => distances[i] = i ? distances[i - 1] + gaps[i] : 0);
-            grades = nodes.map(([x, y, z], i) => i ? (z - nodes[i - 1][2]) / gaps[i] : 0);
-            console.info({gaps, grades}, gaps.reduce((agg, x) => agg + x, 0));
-            markAnimationDuration = 400;
-            elevationProfile.setOption({series: [{
-                data: nodes.map((x, i) => [distances[i], x[2], grades[i]]),
+            chart.setOption({xAxis: {inverse: reverse}});
+            markAnimationDuration = 20; // reset to render is not uber-slow
+            chart.setOption({series: [{
+                data: road.coords.map((x, i) => [road.distances[i], road.elevations[i], road.grades[i]]),
             }]});
         }
-        elevationProfile.setOption({series: [{
+        chart.setOption({series: [{
             markPoint: {
                 itemStyle: {borderColor: '#000'},
                 animationDurationUpdate: markAnimationDuration,
                 animationEasingUpdate: 'linear',
                 data: nearby.filter(x => x.state.roadId === road.id && x.state.reverse === reverse).map(x => {
-                    const fracIdx = distances.length * (x.state.roadCompletion / 1000000);
-                    const nextGap = gaps[fracIdx | 0 + 1] || 0;
-                    const distance = distances[fracIdx | 0] + nextGap * (fracIdx % 1);
-                    const yPos = Math.max(
-                        x.state.altitude,
-                        nodes[fracIdx | 0][2],
-                        nextGap ? nodes[fracIdx | 0 + 1][2] : -Infinity
-                    ) + 2;
+                    // XXX
+                    const distances = road.coords.map(c => vectorDistance(c, [x.state.x, x.state.y, x.state.z]));
+                    const nearest = distances.indexOf(Math.min(...distances));
+                    const distance = road.distances[nearest];
+                    if (x.watching) {
+                        //console.log(nearest, distance, distances);
+                    }
                     return {
                         name: x.athleteId,
-                        coord: [distance, yPos],
+                        coord: [distance, x.state.altitude + 2],
                         symbolSize: x.watching ? 40 : 20,
                         itemStyle: {
                             color: x.watching ? '#f54e' : '#fff6',
@@ -224,27 +126,115 @@ export async function main() {
                 }),
             },
         }]});
-        markAnimationDuration = Math.min(2000, markAnimationDuration * 1.2);
+        //markAnimationDuration = Math.min(1200, markAnimationDuration * 1.3);
     });
+    return chart;
+}
+
+
+async function createMapCanvas(renderer) {
+    const tileSize = 4096;
+    const canvas = document.querySelector('.map-canvas > canvas');
+    const ctx = canvas.getContext('2d');
+    const worldList = await (await fetch('/shared/deps/data/worldlist.json')).json();
+    let courseId;
+    let worldMeta;
+    let roads;
+    let reset;
+    const dots = new Map();
+    renderer.addCallback(async nearby => {
+        if (!nearby || !nearby.length) {
+            return;
+        }
+        const watching = nearby.find(x => x.watching);
+        if (watching.state.courseId !== courseId) {
+            courseId = watching.state.courseId;
+            worldMeta = worldList.find(x => x.courseId === courseId);
+            const worldId = common.courseToWorldIds[courseId];
+            roads = (await (await fetch(`/shared/deps/data/worlds/${worldId}/roads.json`)).json());
+            let xStart = Infinity, xEnd = -Infinity, yStart = Infinity, yEnd = -Infinity;
+            for (const m of worldMeta.minimap) {
+                const size = m.scale * tileSize;
+                xEnd = Math.max(xEnd, tileSize * m.xOffset + size);
+                yEnd = Math.max(yEnd, tileSize * m.yOffset + size);
+                xStart = Math.min(xStart, tileSize * m.xOffset);
+                yStart = Math.min(yStart, tileSize * m.yOffset);
+            }
+            const width = canvas.width = xEnd - xStart;
+            const height = canvas.height = yEnd - yStart;
+            ctx.save();
+            ctx.clearRect(0, 0, width, height);
+            ctx.imageSmoothingQuality = 'high';
+            const img = new Image();
+            const vertDir = worldMeta.flippedHack ? -1 : 1;
+            for (const tile of worldMeta.minimap) {
+                img.src = `/shared/deps/data/worlds/${worldId}/${tile.file}`;
+                await img.decode();
+                ctx.save();
+                ctx.drawImage(img,
+                    tile.xOffset * tileSize - xStart,
+                    tile.yOffset * tileSize - yStart,
+                    tileSize * tile.scale,
+                    tileSize * tile.scale);
+                ctx.restore();
+                await sauce.sleep(100);
+            }
+            ctx.restore();
+            if (worldId === 1) {
+                document.querySelector('.map-canvas').style.transform = 'transform: rotate(90deg) translate(-933px, 3716px);';
+            }
+
+
+            /* Save because has magical innsbrook numbers... :( XXX
+             *           watopia magic: -933px, 3716px;
+            reset = () => {
+                ctx.restore();
+                ctx.clearRect(0, 0, width, height);
+                ctx.save();
+                ctx.scale(1, -1);
+                ctx.drawImage(img, 0, -height);
+                ctx.restore();
+                ctx.save();
+                ctx.translate(1415, 1006);
+            };
+            */
+        }
+        //reset();
+        for (const x of nearby) {
+            if (!dots.has(x.athleteId) || true) {
+                const dot = document.createElement('div');
+                dot.classList.add('dot');
+                dot.classList.toggle('watching', !!x.watching);
+                dot.dataset.athleteId = x.athleteId;
+                document.querySelector('.map-canvas').append(dot);
+                dots.set(x.athleteId, dot);
+            }
+            const dot = dots.get(x.athleteId);
+            dot.lastSeen = Date.now();
+            dot.style.setProperty('--x', `${(x.state.x / worldMeta.tileScale) * tileSize}px`);
+            dot.style.setProperty('--y', `${(x.state.y / worldMeta.tileScale) * tileSize}px`);
+        }
+        for (const r of Object.values(roads)) {
+            for (const [x, y] of r.coords) {
+                const dot = document.createElement('div');
+                dot.classList.add('dot');
+                document.querySelector('.map-canvas').append(dot);
+                dot.style.setProperty('--x', `${(x / worldMeta.tileScale) * tileSize}px`);
+                dot.style.setProperty('--y', `${(y / worldMeta.tileScale) * tileSize}px`);
+            }
+        }
+    });
+}
+
+
+export async function main() {
+    common.initInteractionListeners();
+    const content = document.querySelector('#content');
+    const renderer = new common.Renderer(content);
+    const elevationProfile = await createElevationProfile(renderer);
+    await createMapCanvas(renderer);
     addEventListener('resize', () => {
         elevationProfile.resize();
-        /*mapChart.resize();
-        mapChart.setOption({
-            graphic: [{
-                type: 'image',
-                id: 'background',
-                left: mapChart.getWidth() * 0.8,
-                top: mapChart.getWidth() * 0.8,
-                z: -10,
-                bounding: 'raw',
-                style: {
-                    image: '/pages/images/maps/world-5.png',
-                    width: mapChart.getWidth() * 0.5,
-                    height: mapChart.getHeight() * 0.5,
-                }
-            }],
-        });
-        */
         renderer.render({force: true});
     });
     common.subscribe('nearby', nearby => {
