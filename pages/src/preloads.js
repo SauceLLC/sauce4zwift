144077f0e7f291c49d1a6c36932e356b211a470d
Justin Mayfield <tooker@gmail.com>
2023-03-03 18:48:51 -0700

    preload compat for web and electron

---
+++ pages/src/preloads.js
@@ -0,0 +1,16 @@
+const fonts = [
+    'fonts/MaterialSymbolsRounded.woff2?v=1',
+    'fonts/Saira.woff2?v=1',
+];
+
+document.head.append(...fonts.map(x => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.href = x;
+    link.as = 'font';
+    link.type = 'font/woff2';
+    if (!window.isElectron) {
+        link.crossOrigin = 'anonymous';
+    }
+    return link;
+}));
