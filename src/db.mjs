692b16364551ff1e874acc58038a7da52b07a598
Justin Mayfield <tooker@gmail.com>
2023-08-12 18:49:06 -0600

    Improve sqlite3 perf with WAL mode

--- src/db.mjs
+++ src/db.mjs
@@ -1,5 +1,6 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
+import process from 'node:process';
 import Database from 'better-sqlite3';
 import {createRequire} from 'node:module';
 const require = createRequire(import.meta.url);
@@ -16,7 +17,9 @@ function getFilename(name) {
 export class SqliteDatabase extends Database {
     constructor(name, {tables, ...options}={}) {
         const filename = getFilename(name);
+        console.info("Opening DB:", filename);
         super(filename, options);
+        this.pragma('journal_mode = WAL');  // improve performance substantially
         for (const [table, schema] of Object.entries(tables)) {
             const schemaText = Object.entries(schema).map(([col, type]) => `${col} ${type}`).join(', ');
             this.prepare(`CREATE TABLE IF NOT EXISTS ${table}(${schemaText});`).run();
@@ -31,3 +34,21 @@ export async function deleteDatabase(name) {
     const filename = getFilename(name);
     await fs.rm(filename, {force: true});
 }
+
+
+function shutdown(origin) {
+    const dbs = Array.from(databases.values());
+    databases.clear();
+    for (const db of dbs) {
+        console.warn(`Closing DB [via ${origin}]:`, db.name);
+        try {
+            db.close();
+        } catch(e) {
+            console.error(e);
+        }
+    }
+}
+
+// NOTE: we don't handle kill signal's because it's only useful
+// in dev and can cause interop problems.
+process.on('exit', shutdown);
