dc5236224a146eb94c247163b5c627d4a93603ef
Justin Mayfield <tooker@gmail.com>
2023-11-30 18:30:13 -0700

    Reflect URL changes of browser-source settings
    Fixes #98

--- pages/src/browser-source.mjs
+++ pages/src/browser-source.mjs
@@ -39,15 +39,17 @@ export function main() {
     const backBtn = document.querySelector('#titlebar .button.back');
     const fwdBtn = document.querySelector('#titlebar .button.forward');
     if (settings.url) {
-        webview.src = settings.url;
-        inputUrl.value = settings.url;
+        load(settings.url);
     }
-    inputUrl.addEventListener('change', () => {
-        if (!inputUrl.value.match(/^[a-z]+:\/\//i)) {
-            inputUrl.value = `https://${inputUrl.value}`;
+    function load(url) {
+        if (!url.match(/^[a-z]+:\/\//i)) {
+            url = `https://${url}`;
         }
-        webview.src = inputUrl.value;
-    });
+        console.debug("Loading:", url);
+        inputUrl.value = url;
+        webview.src = url;
+    }
+    inputUrl.addEventListener('change', () => load(inputUrl.value));
     function onDidNav(url) {
         inputUrl.value = url;
         pinBtn.classList.toggle('pinned', url === settings.url);
@@ -93,7 +95,13 @@ export function main() {
     }
     setBackground();
     setOpacity();
+    let reloadTimeout;
     common.settingsStore.addEventListener('changed', ev => {
+        if (ev.data.changed.has('url')) {
+            clearTimeout(reloadTimeout);
+            reloadTimeout = setTimeout(() => load(settings.url), 2000);
+            inputUrl.value = settings.url;
+        }
         setBackground();
         setOpacity();
     });
@@ -103,4 +111,12 @@ export function main() {
 export async function settingsMain() {
     common.initInteractionListeners();
     await common.initSettingsForm('form')();
+}
+
+
+const q = new URL(import.meta.url).searchParams;
+if (q.has('main')) {
+    main();
+} else if (q.has('settings')) {
+    settingsMain();
 }
