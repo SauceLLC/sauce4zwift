d1af847fcec5e42c265964890f575ce9b6543388
Justin Mayfield <tooker@gmail.com>
2024-05-15 21:29:04 -0600

    Add a port of webkit's bezier func used for anim timing func
    I want to support cubic bezier easing functions exactly like CSS but
    I need a fast solver for the cubic bezier.

--- shared/curves.mjs
+++ shared/curves.mjs
@@ -31,7 +31,7 @@ export function lerp(t, a, b) {
 }
 
 
-export function computeBezier(t, a, b, c, d) {
+export function computeBezier3d(t, a, b, c, d) {
     const T = 1 - t;
     const x = T * T * T * a[0]
         + 3 * T * T * t * b[0]
@@ -47,6 +47,164 @@ export function computeBezier(t, a, b, c, d) {
         + t * t * t * d[2];
     return [x, y, z];
 }
+
+
+export function computeBezier2d(t, a, b, c, d) {
+    const T = 1 - t;
+    const x = T * T * T * a[0]
+        + 3 * T * T * t * b[0]
+        + 3 * T * t * t * c[0]
+        + t * t * t * d[0];
+    const y = T * T * T * a[1]
+        + 3 * T * T * t * b[1]
+        + 3 * T * t * t * c[1]
+        + t * t * t * d[1];
+    return [x, y];
+}
+
+
+// Ported from webkit:
+// https://github.com/WebKit/WebKit/blob/main/Source/WebCore/platform/graphics/UnitBezier.h
+export class UnitBezier {
+
+    _samplesSize = 11;
+    _bezierEpsilon = 1e-7;
+    _maxNewtonIters = 4;
+
+    constructor(p1x, p1y, p2x, p2y) {
+        // Calculate the polynomial coefficients, implicit first and last control points
+        // are (0,0) and (1,1).
+        this.cx = 3.0 * p1x;
+        this.bx = 3.0 * (p2x - p1x) - this.cx;
+        this.ax = 1.0 - this.cx - this.bx;
+
+        this.cy = 3.0 * p1y;
+        this.by = 3.0 * (p2y - p1y) - this.cy;
+        this.ay = 1.0 - this.cy - this.by;
+
+        // End-point gradients are used to calculate timing function results
+        // outside the range [0, 1].
+        //
+        // There are four possibilities for the gradient at each end:
+        // (1) the closest control point is not horizontally coincident with regard to
+        //     (0, 0) or (1, 1). In this case the line between the end point and
+        //     the control point is tangent to the bezier at the end point.
+        // (2) the closest control point is coincident with the end point. In
+        //     this case the line between the end point and the far control
+        //     point is tangent to the bezier at the end point.
+        // (3) both internal control points are coincident with an endpoint. There
+        //     are two special case that fall into this category:
+        //     CubicBezier(0, 0, 0, 0) and CubicBezier(1, 1, 1, 1). Both are
+        //     equivalent to linear.
+        // (4) the closest control point is horizontally coincident with the end
+        //     point, but vertically distinct. In this case the gradient at the
+        //     end point is Infinite. However, this causes issues when
+        //     interpolating. As a result, we break down to a simple case of
+        //     0 gradient under these conditions.
+        if (p1x > 0) {
+            this.startGradient = p1y / p1x;
+        } else if (!p1y && p2x > 0) {
+            this.startGradient = p2y / p2x;
+        } else if (!p1y && !p2y) {
+            this.startGradient = 1;
+        } else {
+            this.startGradient = 0;
+        }
+        if (p2x < 1) {
+            this.endGradient = (p2y - 1) / (p2x - 1);
+        } else if (p2y === 1 && p1x < 1) {
+            this.endGradient = (p1y - 1) / (p1x - 1);
+        } else if (p2y === 1 && p1y === 1) {
+            this.endGradient = 1;
+        } else {
+            this.endGradient = 0;
+        }
+        this.splineSamples = [];
+        const deltaT = 1.0 / (this._samplesSize - 1);
+        for (let i = 0; i < this._samplesSize; i++) {
+            this.splineSamples.push(this.sampleCurveX(i * deltaT));
+        }
+    }
+
+    sampleCurveX(t) {
+        // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
+        return ((this.ax * t + this.bx) * t + this.cx) * t;
+    }
+
+    sampleCurveY(t) {
+        return ((this.ay * t + this.by) * t + this.cy) * t;
+    }
+
+    sampleCurveDerivativeX(t) {
+        return (3.0 * this.ax * t + 2.0 * this.bx) * t + this.cx;
+    }
+
+    // Given an x value, find a parametric value it came from.
+    solveCurveX(x, epsilon=1e-6) {
+        let t0 = 0.0;
+        let t1 = 0.0;
+        let t2 = x;
+        let x2 = 0.0;
+        let d2 = 0.0;
+        // Linear interpolation of spline curve for initial guess.
+        const deltaT = 1.0 / (this._samplesSize - 1);
+        for (let i = 1; i < this._samplesSize; i++) {
+            if (x <= this.splineSamples[i]) {
+                t1 = deltaT * i;
+                t0 = t1 - deltaT;
+                t2 = t0 + (t1 - t0) * (x - this.splineSamples[i - 1]) /
+                    (this.splineSamples[i] - this.splineSamples[i - 1]);
+                break;
+            }
+        }
+
+        // Perform a few iterations of Newton's method -- normally very fast.
+        // See https://en.wikipedia.org/wiki/Newton%27s_method.
+        const newtonEpsilon = Math.min(this._bezierEpsilon, epsilon);
+        for (let i = 0; i < this._maxNewtonIters; i++) {
+            x2 = this.sampleCurveX(t2) - x;
+            if (Math.abs(x2) < newtonEpsilon) {
+                return t2;
+            }
+            d2 = this.sampleCurveDerivativeX(t2);
+            if (Math.abs(d2) < this._bezierEpsilon) {
+                break;
+            }
+            t2 = t2 - x2 / d2;
+        }
+        if (Math.abs(x2) < epsilon) {
+            return t2;
+        }
+        // Fall back to the bisection method for reliability.
+        while (t0 < t1) {
+            x2 = this.sampleCurveX(t2);
+            if (Math.abs(x2 - x) < epsilon) {
+                return t2;
+            }
+            if (x > x2) {
+                t0 = t2;
+            } else {
+                t1 = t2;
+            }
+            t2 = (t1 + t0) * 0.5;
+        }
+        // Failure.
+        return t2;
+    }
+
+    solve(x, epsilon) {
+        if (x < 0.0) {
+            return 0.0 + this.startGradient * x;
+        }
+        if (x > 1.0) {
+            return 1.0 + this.endGradient * (x - 1.0);
+        }
+        return this.sampleCurveY(this.solveCurveX(x, epsilon));
+    }
+}
+
+
+export const computeBezier = computeBezier3d;
 
 
 export function splitBezier(t, start, cp1, cp2, end) {
