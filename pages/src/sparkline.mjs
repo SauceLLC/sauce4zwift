5fae38926462030c313b892a0b5e9ea0f8c68c50
Justin Mayfield <tooker@gmail.com>
2024-11-26 13:40:13 -0700

    Moh betta sparkline animation

--- pages/src/sparkline.mjs
+++ pages/src/sparkline.mjs
@@ -31,15 +31,18 @@ export class Sparkline {
             this._boxHeight = Math.round(rect.height * 2);
             this._boxWidth = Math.round(this._boxHeight * ar);
         }
-        this._svg.setAttribute('viewBox', `0 0 ${this._boxWidth} ${this._boxHeight}`);
+        this._svgEl.setAttribute('viewBox', `0 0 ${this._boxWidth} ${this._boxHeight}`);
         if (forceRender) {
             this.render();
         }
     }
 
     setElement(el) {
-        this._resizeObserver.disconnect();
-        this._resizeObserver.observe(el);
+        const old = this.el;
+        if (old) {
+            this._resizeObserver.unobserve(old);
+            old.removeEventListener('hover', this.onHoverForTooltips);
+        }
         this.el = el;
         this.el.innerHTML = `
             <svg class="sauce-sparkline"
@@ -52,10 +55,13 @@ export class Sparkline {
                 </g>
             </svg>
         `;
-        this._svg = this.el.querySelector('svg');
-        this._points = this.el.querySelector('g.points');
-        this._path = this.el.querySelector('path.data');
+        this._svgEl = this.el.querySelector('svg');
+        this._pointsEl = this.el.querySelector('g.points');
+        this._pathEl = this.el.querySelector('path.data');
+        this._pointsMap = new Map();
         this._adjustAspectRatio();
+        this.el.addEventListener('hover', this.onHoverForTooltips);
+        this._resizeObserver.observe(el);
     }
 
     setData(data) {
@@ -83,67 +89,118 @@ export class Sparkline {
         return norm;
     }
 
-    render() {
-        if (!this.data || !this.data.length) {
-            this._path.style.removeProperty('d');
-            this._points.innerHTML = '';
+    onHoverForTooltips(ev) {
+        const point = ev.target.closest('circle.data-point');
+        if (!point) {
             return;
         }
-        const nd = this.normalizeData(this.data);
-        const yMin = this.yMin != null ? this.yMin : Math.min(...nd.map(o => o.y));
-        const yMax = this.yMax != null ? this.yMax : Math.max(...nd.map(o => o.y));
+    }
+
+    async render() {
+        if (!this.data || !this.data.length) {
+            this._pathEl.style.removeProperty('d');
+            this._pointsEl.innerHTML = '';
+            this._pointsMap.clear();
+            return;
+        }
+        const {coords, normalized} = this._renderData();
+        const {pointUpdates} = this._renderStageUpdates(coords, normalized);
+        this._renderFinal(coords, pointUpdates);
+        this._prevCoords = coords;
+    }
+
+    _renderData() {
+        // Step 1: data processing
+        const normalized = this.normalizeData(this.data);
+        let yND;
+        const yMin = this.yMin != null ? this.yMin : Math.min(...(yND = normalized.map(o => o.y)));
+        const yMax = this.yMax != null ? this.yMax : Math.max(...(yND || normalized.map(o => o.y)));
         const yRange = (yMax - yMin) || 1;
-        const xMin = this.xMin != null ? this.xMin : nd[0].x;
-        const xMax = this.xMax != null ? this.xMax : nd[nd.length - 1].x;
+        const xMin = this.xMin != null ? this.xMin : normalized[0].x;
+        const xMax = this.xMax != null ? this.xMax : normalized[normalized.length - 1].x;
         const xRange = (xMax - xMin) || 1;
         const vPad = this.padding[0] + this.padding[2];
         const hPad = this.padding[1] + this.padding[3];
-        const d = [];
-        const points = [];
-        for (let i = 0; i < nd.length; i++) {
-            const o = nd[i];
-            const x = ((o.x - xMin) / xRange * (this._boxWidth - hPad));
-            const y = (this._boxHeight - vPad) - ((o.y - yMin) / yRange * (this._boxHeight - vPad));
-            d.push([x, y]);
-            const title = o.tooltip ? o.tooltip(o) : this.onTooltip ? this.onTooltip(o) : y.toLocaleString();
-            points.push({x, y, title});
-        }
-        if (this._prevPathD) {
+        const coords = normalized.map(o => [
+            (o.x - xMin) / xRange * (this._boxWidth - hPad),
+            (this._boxHeight - vPad) - ((o.y - yMin) / yRange * (this._boxHeight - vPad))
+        ]);
+        return {coords, normalized};
+    }
+
+    _renderStageUpdates(coords, normalized) {
+        let needForceLayout;
+        if (this._prevCoords) {
             // We can use CSS to animate the transition but we have to use a little hack because it only
             // animates when the path has the same number of points.
-            if (this._prevPathD.length !== d.length) {
-                const prev = Array.from(this._prevPathD);
-                while (prev.length > d.length) {
+            if (this._prevCoords.length !== coords.length) {
+                const prev = Array.from(this._prevCoords);
+                while (prev.length > coords.length) {
                     prev.shift(); // XXX assumption that data is going to move right
                 }
-                while (prev.length < d.length) {
+                while (prev.length < coords.length) {
                     prev.push(prev[prev.length - 1]);
                 }
-                this._path.style.setProperty('d', `path('M${prev.map((o, i) => o.join()).join(' ')}')`);
-                this._path.clientWidth;
+                this._pathEl.style.setProperty('d', `path('M${prev.map((o, i) => o.join()).join(' ')}')`);
+                needForceLayout = true;
             }
         }
-        this._path.style.setProperty('d', `path('M${d.map((o, i) => o.join()).join(' ')}')`);
-        this._prevPathD = d;
         let i;
-        while (this._points.children.length > i) {
-            this._points.children[0].remove();
+        const remPoints = new Set(this._pointsMap.values());
+        const newPoints = [];
+        const pointUpdates = [];
+        for (i = 0; i < coords.length; i++) {
+            const coord = coords[i];
+            const dataRef = this.data[i];
+            let point = this._pointsMap.get(dataRef);
+            if (!point) {
+                const ndRef = normalized[i];
+                point = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+                point._dataRef = dataRef;
+                point.classList.add('data-point');
+                point._tooltipFormat = ndRef.tooltip ?
+                    ndRef.tooltip :
+                    this.onTooltip ?
+                        this.onTooltip :
+                        () => ndRef.y.toLocaleString();
+                this._pointsMap.set(dataRef, point);
+                console.warn("adding", point, i, '/', coords.length);
+                if (i && i === coords.length - 1 && this._prevCoords) {
+                    // animate it in...
+                    const p = this._prevCoords[this._prevCoords.length - 1];
+                    point.setAttribute('cx', p[0]);
+                    point.setAttribute('cy', p[1]);
+                    this._pointsEl.append(point);
+                    point.clientWidth; // force layout
+                } else {
+                    newPoints.push(point);
+                }
+            } else {
+                remPoints.delete(point);
+            }
+            const sig = coord.join();
+            if (point._sig !== sig) {
+                pointUpdates.push([point, coord]);
+                point._sig = sig;
+            }
         }
-        for (i = 0; i < points.length; i++) {
-            const o = points[i];
-            let circle = this._points.children[i];
-            if (!circle) {
-                circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-                circle.innerHTML = '<title></title>';
-                this._points.append(circle);
-            }
-            const sig = JSON.stringify(o);
-            if (circle._sig !== sig) {
-                circle.setAttribute('cx', o.x);
-                circle.setAttribute('cy', o.y);
-                circle.querySelector('title').innerHTML = o.title;
-                circle._sig = sig;
-            }
+        for (const x of remPoints) {
+            this._pointsMap.delete(x._dataRef);
+            x.remove();
+        }
+        this._pointsEl.append(...newPoints);
+        if (needForceLayout) {
+            this._pathEl.clientWidth;
+        }
+        return {pointUpdates};
+    }
+
+    _renderFinal(coords, pointUpdates) {
+        this._pathEl.style.setProperty('d', `path('M${coords.map((o, i) => o.join()).join(' ')}')`);
+        for (let i = 0; i < pointUpdates.length; i++) {
+            const [point, coord] = pointUpdates[i];
+            point.setAttribute('cx', coord[0]);
+            point.setAttribute('cy', coord[1]);
         }
     }
 }
