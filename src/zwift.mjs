ec7815d5f5fc615cb87cf32a3bb1ef48faa2a785
Justin Mayfield <tooker@gmail.com>
2025-07-23 17:52:01 -0600

    do not send eventSubgroupId = 0
    The relay network does but this just bit me in the butt, so I'm setting
    it to null by hand.

--- src/zwift.mjs
+++ src/zwift.mjs
@@ -262,6 +262,9 @@ export function processPlayerStateMessage(msg) {
     // Route ID can be stale in a few situations.  This may change but so far it looks like when
     // progress hits 100% and routeProgess rollsover to 0 the route is no longer correct.
     const routeId = msg.portal || (progress === 1 && msg.routeProgress === 0) ? undefined : msg.routeId;
+    if (msg.eventSubgroupId === 0) {
+        msg.eventSubgroupId = null;
+    }
     return {
         ...msg,
         ...flags1,
@@ -1854,7 +1857,7 @@ export class GameMonitor extends events.EventEmitter {
                         watchingAthleteId: this.watchingAthleteId,
                         _flags2: portal ? encodePlayerStateFlags2({roadId: lws.roadId}) : undefined,
                         portal,
-                        eventSubgroupId: lws?.eventSubgroupId,
+                        eventSubgroupId: lws?.eventSubgroupId || 0,
                         ...this.watchingStateExtra});
                     break;
                 } catch(e) {
