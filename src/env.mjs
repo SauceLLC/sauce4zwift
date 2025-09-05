dae3302b8beb8d6fbf715a25ea0a5074deced6a9
Justin Mayfield <tooker@gmail.com>
2025-09-05 14:57:11 -0600

    Add env.getRouteRoadSections
    Design still in flux, but this is the meat of the work previously
    handled by segment projection.  Final design will probably eliminate or
    simplify the segment projection and use this core func as the basis
    instead.  Segment projection will take place in the UI code and
    then in a different form on the backend for TBD "To-Next" field(s).

--- src/env.mjs
+++ src/env.mjs
@@ -207,7 +207,7 @@ export function getRoad(courseId, roadId) {
 }
 
 
-export function getRoadCurvePath(courseId, roadId) {
+export function getRoadCurvePath(courseId, roadId, options) {
     const sig = `${courseId}-${roadId}`;
     if (!_roadCurvePaths.has(sig)) {
         const road = getRoad(courseId, roadId);
@@ -219,7 +219,7 @@ export function getRoadCurvePath(courseId, roadId) {
                 CatmullRom: curves.catmullRomPath,
                 Bezier: curves.cubicBezierPath,
             }[road.splineType];
-            rcp = curveFunc(road.path, {loop: road.looped, road: true});
+            rcp = curveFunc(road.path, {loop: road.looped, road: true, ...options});
         }
         _roadCurvePaths.set(sig, rcp);
     }
@@ -266,7 +266,129 @@ function readRoutes() {
 }
 
 
+export function getRouteRoadSections(route, {epsilon=routeDistEpsilon}={}) {
+    const sections = route.manifest.map(x => ({
+        roadId: x.roadId,
+        reverse: !!x.reverse,
+        leadin: !!x.leadin,
+        weld: false,
+        roadCurvePath: getRoadCurvePath(route.courseId, x.roadId, {epsilon})
+            .subpathAtRoadPercents(x.start, x.end),
+        distance: 0,
+        blockOffsetDistance: 0,
+        marginStartDistance: 0,
+        marginEndDistance: 0,
+    }));
+    const fullPath = new curves.CurvePath({epsilon});
+    const hasLeadin = route.manifest[0].leadin;
+    let leadinDist = 0;
+    for (const [i, m] of route.manifest.entries()) {
+        const section = sections[i];
+        const distToEndOfLast = fullPath.distance() / 100;
+        const road = section.roadCurvePath;
+        if (i) {
+            // Include the margin between the last road's end and our start.
+            fullPath.extend(m.reverse ? road.slice(-1) : road.slice(0, 1));
+        }
+        const distToStartOfThis = fullPath.distance() / 100;
+        fullPath.extend(m.reverse ? road.toReversed() : road);
+        const distToEndOfThis = fullPath.distance() / 100;
+        section.marginStartDistance = distToStartOfThis - distToEndOfLast;
+        section.distance = distToEndOfThis - distToStartOfThis;
+        if (!leadinDist && hasLeadin && !m.leadin) {
+            leadinDist = distToStartOfThis;
+            section.blockOffsetDistance = 0;
+        } else {
+            if (m.leadin) {
+                section.blockOffsetDistance = distToStartOfThis;
+            } else {
+                section.blockOffsetDistance = distToStartOfThis - leadinDist;
+            }
+        }
+        if (i) {
+            sections[i - 1].marginEndDistance = distToStartOfThis - distToEndOfLast;
+        }
+    }
+    if (route.supportedLaps) {
+        // Handle cases where route data does not properly weld the lap together, (e.g Three Little Sister)
+        const lapStart = route.manifest.find(x => !x.leadin);
+        const lapEnd = route.manifest.at(-1);
+        if (lapStart.roadId !== lapEnd.roadId || lapStart.reverse !== lapEnd.reverse) {
+            console.warn("Unable to properly weld lap together for:", route.id);
+            const startNode = sections[route.manifest.indexOf(lapStart)].roadCurvePath.nodes[0].end;
+            const endNode = sections.at(-1).nodes.at(-1).roadCurvePath.end;
+            sections.push({
+                roadId: null,
+                reverse: null,
+                leadin: false,
+                weld: true,
+                roadCurvePath: null,
+                distance: curves.vecDist(endNode, startNode) / 100,
+                blockOffsetDistance: 0,
+                marginStartDistance: 0,
+                marginEndDistance: 0,
+            });
+        } else {
+            let start, end;
+            if (!lapStart.reverse) {
+                start = lapEnd.end;
+                end = lapStart.start;
+            } else {
+                start = lapEnd.start;
+                end = lapStart.end;
+            }
+            if (Math.abs(start - end) > 1e-4) {
+                const road = getRoadCurvePath(route.courseId, lapStart.roadId, {epsilon});
+                if (start > end) {
+                    const w1 = road.subpathAtRoadPercents(start, 1);
+                    const w2 = road.subpathAtRoadPercents(0, end);
+                    const w1Dist = w1.distance() / 100;
+                    const w2Dist = w2.distance() / 100;
+                    sections.push({
+                        roadId: lapStart.roadId,
+                        reverse: !!lapStart.reverse,
+                        leadin: false,
+                        weld: true,
+                        roadCurvePath: w1,
+                        distance: w1Dist,
+                        blockOffsetDistance: 0,
+                        marginStartDistance: 0,
+                        marginEndDistance: 0,
+                    }, {
+                        roadId: lapStart.roadId,
+                        reverse: !!lapStart.reverse,
+                        leadin: false,
+                        weld: true,
+                        roadCurvePath: w2,
+                        distance: w2Dist,
+                        blockOffsetDistance: w1Dist,
+                        marginStartDistance: 0,
+                        marginEndDistance: 0,
+                    });
+                } else {
+                    const w = road.subpathAtRoadPercents(start, end);
+                    const wDist = w.distance() / 100;
+                    sections.push({
+                        roadId: lapStart.roadId,
+                        reverse: !!lapStart.reverse,
+                        leadin: false,
+                        weld: true,
+                        roadCurvePath: w,
+                        distance: wDist,
+                        blockOffsetDistance: 0,
+                        marginStartDistance: 0,
+                        marginEndDistance: 0,
+                    });
+                }
+            }
+        }
+    }
+    return sections;
+}
+
+
 export function projectRouteSegments(route, {laps=1, distance, epsilon=routeDistEpsilon}={}) {
+    // XXX refactor to use getRouteRoadSections
     let state = _routeSegmentCache.get(route.id);
     if (!state) {
         _routeSegmentCache.set(route.id, (state = {}));
