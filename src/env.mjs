117ac840c57a92369b17e661f7fc0e931753a55c
Justin Mayfield <tooker@gmail.com>
2026-02-15 22:20:30 -0700

    segment projections redux - draft
    loop support, unoptimized but conformant.

--- src/env.mjs
+++ src/env.mjs
@@ -1,7 +1,9 @@
 import path from 'node:path';
 import fs from './fs-safe.js';
 import * as curves from '../shared/curves.mjs';
+import * as Routes from '../shared/routes.mjs';
 import {fileURLToPath} from 'node:url';
+import Assert from 'node:assert/strict';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,7 +14,6 @@ const _routes = new Map();
 const _roads = new Map();
 const _roadsByCourse = new Map();
 const _roadCurvePaths = new Map();
-const _routeSegmentCache = new Map();
 const _coursesByWorld = new Map();
 
 export const realWorldCourseId = -2;
@@ -83,50 +84,6 @@ export function fromRoadSig(roadSig) {
 }
 
 
-function readSegmentsForWorldLegacy(worldId) {
-    const fname = path.join(__dirname, `../shared/deps/data/worlds/${worldId}/segments.json`);
-    let data;
-    try {
-        data = JSON.parse(fs.readFileSync(fname));
-    } catch(e) {
-        console.error('No segments loaded for world:', worldId);
-        return [];
-    }
-    const segments = [];
-    const courseId = getCourseId(worldId);
-    for (const x of data) {
-        for (const dir of ['Forward', 'Reverse']) {
-            if (!x['id' + dir]) {
-                continue;
-            }
-            const reverse = dir === 'Reverse';
-            const segment = {
-                ...x,
-                reverse,
-                courseId,
-                id: x['id' + dir],
-                distance: x['distance' + dir],
-                name: reverse ? x.nameReverse || x.nameForward + ' Reverse' : x.nameForward,
-                roadStart: x['roadStart' + dir],
-            };
-            delete segment.nameForward;
-            delete segment.nameReverse;
-            delete segment.idForward;
-            delete segment.idReverse;
-            delete segment.distanceForward;
-            delete segment.distanceReverse;
-            delete segment.roadStartForward;
-            delete segment.roadStartReverse;
-            if (!segment.distance) {
-                continue;  // exclude single direction segments
-            }
-            segments.push(segment);
-        }
-    }
-    return segments;
-}
-
-
 function readSegmentsForWorld(worldId) {
     const fname = path.join(__dirname, `../shared/deps/data/worlds/${worldId}/segments.json`);
     let segments;
@@ -139,6 +96,13 @@ function readSegmentsForWorld(worldId) {
     const courseId = getCourseId(worldId);
     for (const x of segments) {
         x.courseId = courseId;
+        try {
+            if (!x.loop) {
+                Assert.ok(x.reverse ? x.roadStart >= x.roadFinish : x.roadStart <= x.roadFinish);
+            } else {
+                Assert.ok(Math.abs(x.roadFinish - x.roadStart) < 0.1);
+            }
+        } catch(e) { debugger; }
     }
     return segments;
 }
@@ -255,18 +219,20 @@ function readRoutes() {
     }
     for (const route of routes) {
         route.courseId = getCourseId(route.worldId);
+        // Legacy segment projection.
+        // Does not handle loops, and segments spanning different section types (leadin, lap, weld).
         for (const m of route.manifest) {
             const segments = [];
             for (const x of _segmentsByCourse.get(route.courseId)) {
                 if (x.roadId === m.roadId && !!x.reverse === !!m.reverse) {
                     if (!x.reverse) {
                         if (x.roadStart >= m.start && x.roadFinish <= m.end &&
-                            (!x.requiresAllCheckpoints || m.end - m.start > 0.90)) {
+                            (!x.loop || m.end - m.start > 0.90)) {
                             segments.push(x);
                         }
                     } else {
                         if (x.roadStart <= m.end && x.roadFinish >= m.start &&
-                            (!x.requiresAllCheckpoints || m.end - m.start > 0.90)) {
+                            (!x.loop || m.end - m.start > 0.90)) {
                             segments.push(x);
                         }
                     }
@@ -278,132 +244,179 @@ function readRoutes() {
                 m.segmentIds = segments.map(x => x.id);
             }
         }
+        route.segmentProjections = projectRouteSegments(route);
     }
     return routes;
 }
 
 
-export function projectRouteSegments(route, {laps=1, distance, epsilon=1/200}={}) {
-    // XXX refactor to use routes.getRouteRoadSections
-    let state = _routeSegmentCache.get(route.id);
-    if (!state) {
-        _routeSegmentCache.set(route.id, (state = {}));
-        const roadSlices = route.manifest.map(x =>
-            getRoadCurvePath(route.courseId, x.roadId).subpathAtRoadPercents(x.start, x.end, {epsilon}));
-        const fullPath = new curves.CurvePath({epsilon});
-        state.routeSegments = [];
-        state.leadinDist = 0;
-        state.lapWeldDistance = 0;
-        const hasLeadin = route.manifest[0].leadin;
-        for (const [i, m] of route.manifest.entries()) {
-            const road = roadSlices[i];
-            if (i) {
-                fullPath.extend(m.reverse ? road.slice(-1) : road.slice(0, 1));
-            }
-            const distBefore = fullPath.distance() / 100;
-            if (!state.leadinDist && hasLeadin && !m.leadin) {
-                state.leadinDist = distBefore;
-            }
-            fullPath.extend(m.reverse ? road.toReversed() : road);
-            if (m.segmentIds) {
-                let distAfter;
-                for (const id of m.segmentIds) {
-                    const segment = getSegment(id);
-                    const start = road.distanceAtRoadPercent(segment.roadStart) / 100;
-                    const end = road.distanceAtRoadPercent(segment.roadFinish) / 100;
-                    let startDistance, endDistance;
-                    if (!segment.reverse) {
-                        startDistance = distBefore + start;
-                        endDistance = distBefore + end;
-                    } else {
-                        distAfter = distAfter || fullPath.distance() / 100;
-                        startDistance = distAfter - start;
-                        endDistance = distAfter - end;
-                    }
-                    state.routeSegments.push({id, startDistance, endDistance, leadin: !!m.leadin});
-                }
-            }
-        }
-        if (route.supportedLaps) {
-            state.lapDist = (fullPath.distance() / 100) - state.leadinDist;
-            /*
-             * Handle cases where route data does not properly weld the lap together
-             *
-             * Case info:
-             *   routeId: 2627606248
-             *   courseId: 6
-             *   name: Three Little Sisters
-             *   problem: Lap finish is several hundred meters away from the leadin offset.
-             */
-            const lapStart = route.manifest.find(x => !x.leadin);
-            const lapEnd = route.manifest.at(-1);
-            if (lapStart.roadId !== lapEnd.roadId || lapStart.reverse !== lapEnd.reverse) {
-                console.warn("Unable to properly weld lap together for:", route.id);
-                const startNode = roadSlices[route.manifest.indexOf(lapStart)].nodes[0].end;
-                const endNode = roadSlices.at(-1).nodes.at(-1).end;
-                state.lapWeldDistance = curves.vecDist(endNode, startNode) / 100;
-            } else {
-                let start, end;
-                if (!lapStart.reverse) {
-                    start = lapEnd.end;
-                    end = lapStart.start;
-                } else {
-                    start = lapEnd.start;
-                    end = lapStart.end;
-                }
-                if (Math.abs(start - end) > 1e-4) {
-                    const road = getRoadCurvePath(route.courseId, lapStart.roadId);
-                    let connection;
-                    if (start > end) {
-                        const joiner = new curves.CurvePath({epsilon});
-                        joiner.extend(road.subpathAtRoadPercents(start, 1));
-                        joiner.extend(road.subpathAtRoadPercents(0, end));
-                        connection = joiner;
-                    } else {
-                        connection = road.subpathAtRoadPercents(start, end, {epsilon});
-                    }
-                    state.lapWeldDistance = connection.distance() / 100;
-                }
-            }
+function rangeOverlap(a, b) {
+    const aHigh = Math.max(...a);
+    const aLow = Math.min(...a);
+    const bHigh = Math.max(...b);
+    const bLow = Math.min(...b);
+    return Math.min(aHigh, bHigh) - Math.max(aLow, bLow);
+}
+
+
+function isInsideRoadSubpath(p, {start, end}, err=-1e-2) {
+    return p - start > err && end - p > err;
+}
+
+
+function projectRouteSegments(route) {
+    const roadCurvePaths = new Map();
+    for (const x of route.manifest) {
+        if (!roadCurvePaths.has(x.roadId)) {
+            roadCurvePaths.set(x.roadId, getRoadCurvePath(route.courseId, x.roadId));
         }
     }
-    const maxLaps = 1000;
-    if (distance) {
-        laps = route.supportedLaps ? Infinity : 1;
-    } else if (laps > 1 && !route.supportedLaps) {
-        console.error("Route does not support laps:", route.id);
-        laps = 1;
-    }
-    const lapSegments = [];
-    if (route.supportedLaps) {
-        for (let lap = 1; lap < laps; lap++) {
-            const lapOfft = (state.lapDist + state.lapWeldDistance) * lap - state.leadinDist;
-            if (distance && state.leadinDist + lapOfft >= distance) {
-                break;
-            }
-            for (const x of state.routeSegments) {
-                if (!x.leadin) {
-                    lapSegments.push({
-                        id: x.id,
-                        startDistance: x.startDistance + lapOfft,
-                        endDistance: x.endDistance + lapOfft,
-                        leadin: false,
-                    });
-                }
-            }
-            if (lap === maxLaps) {
-                console.error(`Reached maximum laps (${maxLaps}) for route:`, route.id);
-                break;
-            }
+    const meta = Routes.getRouteMeta(route, {roadCurvePaths});
+    const matchRequirement = 0.98;
+    const segments = [];
+
+    const getOffset = (section, roadPct) => {
+        const blockPrelude = section.leadin ?
+            0 :
+            section.weld ?
+                meta.lapDistance :
+                meta.leadinDistance;
+        let sOfft = section.roadCurvePath.distanceAtRoadPercent(roadPct) / 100;
+        if (section.reverse) {
+            sOfft = section.distance - sOfft;
         }
-    }
-    const allSegments = state.routeSegments.concat(lapSegments);
-    return {
-        leadinDistance: state.leadinDist,
-        lapDistance: state.lapDist,
-        lapWeldDistance: state.lapWeldDistance,
-        segments: distance ? allSegments.filter(x => x.endDistance <= distance) : allSegments,
+        return blockPrelude + section.blockOffsetDistance + sOfft;
     };
+
+    // NOTE: I've tried various algos here that normalize the inputs, then do less overlap
+    // testing.  However it just makes all the test cases harder to verify and the code uglier.
+    // So this is heavily branched, or written like test case code, to make it less difficult
+    // to reason about..
+    for (const [mi, m] of route.manifest.entries()) {
+        for (const s of _segmentsByCourse.get(route.courseId)) {
+            if (s.roadId !== m.roadId || !!s.reverse !== !!m.reverse) {
+                continue;
+            }
+            let startDistance, distance;
+            if (!s.loop) {
+                const minOverlap = matchRequirement * Math.abs(s.roadFinish - s.roadStart);
+                const overlap = rangeOverlap([s.roadStart, s.roadFinish], [m.start, m.end]);
+                if (!s.reverse) {
+                    if (overlap >= minOverlap) {
+                        const sec = meta.sections[mi];
+                        startDistance = getOffset(sec, s.roadStart);
+                        distance = getOffset(sec, s.roadFinish) - startDistance;
+                    } else if (overlap >= 0) {
+                        // check if we do overlap if joined with next manifest entry..
+                        const m2 = route.manifest[mi + 1];
+                        if (m2 && m2.roadId === s.roadId && !!m2.reverse === !!s.reverse &&
+                            m2.start >= m.end) {
+                            const multiOverlap = rangeOverlap([s.roadStart, s.roadFinish], [m.start, m2.end]);
+                            if (multiOverlap >= minOverlap) {
+                                const sec = meta.sections[mi];
+                                const sec2 = meta.sections[mi + 1];
+                                startDistance = getOffset(sec, s.roadStart);
+                                distance = getOffset(sec2, s.roadFinish) - startDistance;
+                            }
+                        }
+                    }
+                } else {
+                    // reverse..
+                    if (overlap >= minOverlap) {
+                        const sec = meta.sections[mi];
+                        startDistance = getOffset(sec, s.roadStart);
+                        distance = getOffset(sec, s.roadFinish) - startDistance;
+                    } else if (overlap >= 0) {
+                        // check if we do overlap if joined with next manifest entry..
+                        const m2 = route.manifest[mi + 1];
+                        if (m2 && m2.roadId === s.roadId && !!m2.reverse === !!s.reverse &&
+                            m2.end <= m.start) {
+                            const multiOverlap = rangeOverlap([s.roadStart, s.roadFinish], [m2.start, m.end]);
+                            if (multiOverlap >= minOverlap) {
+                                const sec = meta.sections[mi];
+                                const sec2 = meta.sections[mi + 1];
+                                startDistance = getOffset(sec, s.roadStart);
+                                distance = getOffset(sec2, s.roadFinish) - startDistance;
+                            }
+                        }
+                    }
+                }
+            } else {
+                // loop
+                const startEndOverlap = !s.reverse ? s.roadStart - s.roadFinish : s.roadFinish - s.roadStart;
+                const minOverlap = matchRequirement * (1 - startEndOverlap);
+                if (isInsideRoadSubpath(s.roadStart, m)) {
+                    if (!s.reverse) {
+                        if (m.end - m.start >= minOverlap) {
+                            const sec = meta.sections[mi];
+                            startDistance = getOffset(sec, s.roadStart);
+                            distance = getOffset(sec, s.roadFinish) - startDistance;
+                            Assert.ok(Math.abs(distance) < 20);
+                            distance += roadCurvePaths.get(s.roadId)
+                                .subpathAtRoadPercents(0, 1)
+                                .distance(Routes.routeDistEpsilon) / 100;
+                        } else {
+                            const m2 = route.manifest[mi + 1];
+                            if (m2 && m2.roadId === s.roadId && !!m2.reverse === !!s.reverse &&
+                                isInsideRoadSubpath(s.roadFinish, m2)) {
+                                if ((m.end - m.start) + (m2.end - m2.start) >= minOverlap) {
+                                    const sec = meta.sections[mi];
+                                    const sec2 = meta.sections[mi + 1];
+                                    startDistance = getOffset(sec, s.roadStart);
+                                    distance = getOffset(sec2, s.roadFinish) - startDistance;
+                                    const continuous = m2.start >= m.end;
+                                    if (continuous) {
+                                        distance += roadCurvePaths.get(s.roadId)
+                                            .subpathAtRoadPercents(0, 1)
+                                            .distance(Routes.routeDistEpsilon) / 100;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        // reverse..
+                        if (m.end - m.start >= minOverlap) {
+                            const sec = meta.sections[mi];
+                            startDistance = getOffset(sec, s.roadStart);
+                            distance = getOffset(sec, s.roadFinish) - startDistance;
+                            Assert.ok(Math.abs(distance) < 20);
+                            distance += roadCurvePaths.get(s.roadId)
+                                .subpathAtRoadPercents(0, 1)
+                                .distance(Routes.routeDistEpsilon) / 100;
+                        } else {
+                            const m2 = route.manifest[mi + 1];
+                            if (m2 && m2.roadId === s.roadId && !!m2.reverse === !!s.reverse &&
+                                isInsideRoadSubpath(s.roadFinish, m2)) {
+                                if ((m.end - m.start) + (m2.end - m2.start) >= minOverlap) {
+                                    const sec = meta.sections[mi];
+                                    const sec2 = meta.sections[mi + 1];
+                                    startDistance = getOffset(sec, s.roadStart);
+                                    distance = getOffset(sec2, s.roadFinish) - startDistance;
+                                    const continuous = m2.end <= m.start;
+                                    if (continuous) {
+                                        distance += roadCurvePaths.get(s.roadId)
+                                            .subpathAtRoadPercents(0, 1)
+                                            .distance(Routes.routeDistEpsilon) / 100;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (startDistance !== undefined) {
+                if (Math.abs(1 - s.distance / distance) > 0.05) {
+                    if (Math.abs(s.distance - distance) > 1000) {
+                        console.error(route.name, s.name, s.distance - distance);
+                    }
+                }
+                const leadin = meta.sections[mi].leadin || undefined;
+                const weld = meta.sections[mi].weld || undefined;
+                segments.push({id: s.id, startDistance, distance, leadin, weld});
+            }
+        }
+    }
+    return segments;
 }
 
 
